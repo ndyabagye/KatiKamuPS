25f8871dd58aa3e107f0d99496bf6df9ea8b7159
ndyabagye <ndyabagyehenrytusi@gmail.com>
2021-12-22 16:32:23 +0300

    first commit

--- src/winBuilder/LoginForm.java
+++ src/winBuilder/LoginForm.java
@@ -17,6 +17,7 @@ import java.awt.Window.Type;
 
 public class LoginForm extends JFrame {
 
+
 	private JPanel contentPane;
 	private JTextField inputEmail;
 	private JTextField inputPassword;
@@ -48,32 +49,32 @@ public class LoginForm extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel Email = new JLabel("Email");
 		Email.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		Email.setBounds(70, 87, 48, 14);
 		contentPane.add(Email);
-		
+
 		JLabel Password = new JLabel("Password");
 		Password.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		Password.setBounds(70, 138, 66, 14);
 		contentPane.add(Password);
-		
+
 		JLabel Heading = new JLabel("KTPS Login");
 		Heading.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		Heading.setBounds(162, 11, 133, 22);
 		contentPane.add(Heading);
-		
+
 		inputEmail = new JTextField();
 		inputEmail.setBounds(162, 82, 160, 27);
 		contentPane.add(inputEmail);
 		inputEmail.setColumns(10);
-		
+
 		inputPassword = new JTextField();
 		inputPassword.setColumns(10);
 		inputPassword.setBounds(162, 133, 160, 27);
 		contentPane.add(inputPassword);
-		
+
 		JButton Cancel = new JButton("Cancel");
 		Cancel.setForeground(new Color(255, 0, 0));
 		Cancel.addActionListener(new ActionListener() {
@@ -83,7 +84,7 @@ public class LoginForm extends JFrame {
 		Cancel.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		Cancel.setBounds(100, 198, 89, 23);
 		contentPane.add(Cancel);
-		
+
 		JButton Submit = new JButton("Login");
 		Submit.setForeground(new Color(0, 0, 255));
 		Submit.setFont(new Font("Tahoma", Font.PLAIN, 12));
