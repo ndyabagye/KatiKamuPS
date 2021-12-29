97696ce018017f68946c381c39b5a0b8ae960aa3
ndyabagye <ndyabagyehenrytusi@gmail.com>
2021-12-29 22:58:30 +0300

    merge fix

--- src/winBuilder/MarksPanel.java
+++ src/winBuilder/MarksPanel.java
@@ -1,5 +1,6 @@
 package winBuilder;
 
+<<<<<<< HEAD
 
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
@@ -7,6 +8,24 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
+=======
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import javax.swing.JMenuBar;
+import javax.swing.JScrollPane;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.JTable;
+>>>>>>> 3bb2250... not much
 import javax.swing.table.DefaultTableModel;
 import javax.swing.*;
 import java.sql.Connection;
@@ -26,12 +45,81 @@ import java.awt.event.*;
 
 public class MarksPanel extends JPanel {
 	private JTable table;
-
+	 
+	 
+	// data that will dynamically change		
+	String[][] data = {};
+	ArrayList<String[]> studentList = new ArrayList<String[]>(Arrays.asList(data));
+	
 	/**
 	 * Create the panel.
 	 */
 	public MarksPanel() {
+<<<<<<< HEAD
 		setLayout(new GridLayout(0, 1, 0, 0));
+=======
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{450, 0};
+		gridBagLayout.rowHeights = new int[]{22, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+	
+		JMenuBar menuBar = new JMenuBar();
+		GridBagConstraints gbc_menuBar = new GridBagConstraints();
+		gbc_menuBar.insets = new Insets(0, 0, 5, 0);
+		gbc_menuBar.fill = GridBagConstraints.BOTH;
+		gbc_menuBar.gridx = 0;
+		gbc_menuBar.gridy = 0;
+		add(menuBar, gbc_menuBar);
+		
+		JButton btnNewButton = new JButton("English");
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				data  = fetchData("English");
+			}
+		});
+		menuBar.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Mathematics");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				data = fetchData("Maths");
+			}
+		});
+		menuBar.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Science");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				data = fetchData("Science");
+			}
+		});
+		menuBar.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("SST");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				data = fetchData("SST");
+				System.out.println(Arrays.deepToString(data));
+			}
+		});
+		menuBar.add(btnNewButton_3);
+		
+>>>>>>> 3bb2250... not much
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane);
 		String[] columns = new String[] {"id","Reg No.", "Name", "Math", "Sci", "SST", "English"};
@@ -72,7 +160,10 @@ public class MarksPanel extends JPanel {
     	}
 		
 		table = new JTable();
+		
+		System.out.println(Arrays.deepToString(data));
 		table.setModel(new DefaultTableModel(
+<<<<<<< HEAD
 			data,
 			columns
 		));
@@ -205,5 +296,66 @@ public class MarksPanel extends JPanel {
             @Override
             public void changedUpdate(DocumentEvent e) {}
         });
+=======
+				data,
+			new String[] {
+				"Reg No.", "First Name", "Last Name", "Marks"
+			}
+		));
+		scrollPane.setViewportView(table);
 	}
+	
+
+
+	private String[][] fetchData(String subj) {
+		try {
+			Connection connection  = new DbConnection().getDbConnection();
+			String query = "SELECT * FROM students WHERE "+ subj +" IS NOT NULL";
+			
+			// create the java statement
+			Statement statement = connection.createStatement();
+			
+			// execute query and get a result set
+			ResultSet rs = statement.executeQuery(query);
+			
+			// clear the array list
+			studentList.clear();
+			
+			// iterate through result set
+			while (rs.next()) {
+				String regNum = rs.getString("regNum");
+				String firstName = rs.getString("firstName");
+				String lastName = rs.getString("lastName");
+				String mark = rs.getString(subj);
+				String [] student = {regNum, firstName, lastName, mark};
+				// add data to the array list
+				studentList.add(student);
+			}
+			data = studentList.toArray(data);
+			
+//			JScrollPane scrollPane = new JScrollPane();
+//			GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+//			gbc_scrollPane.fill = GridBagConstraints.BOTH;
+//			gbc_scrollPane.gridx = 0;
+//			gbc_scrollPane.gridy = 1;
+//			add(scrollPane, gbc_scrollPane);
+//			
+//			table = new JTable();
+//			
+//			table.setModel(new DefaultTableModel(
+//					data,
+//				new String[] {
+//					"Reg No.", "First Name", "Last Name", "Marks"
+//				}
+//			));
+//			scrollPane.setViewportView(table);
+			connection.close();
+		}catch(Exception exception) {
+			exception.printStackTrace();
+		};
+				
+		return data;
+>>>>>>> 3bb2250... not much
+	}
+	
 }
