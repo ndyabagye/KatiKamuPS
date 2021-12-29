11f3d929bf633bc3c92ef3a4658198a21f5ff652
ndyabagye <ndyabagyehenrytusi@gmail.com>
2021-12-29 12:59:10 +0300

    remove unused inports

--- src/winBuilder/RegisterStudentForm.java
+++ src/winBuilder/RegisterStudentForm.java
@@ -1,6 +1,5 @@
 package winBuilder;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -14,15 +13,9 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import javax.swing.JFormattedTextField;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.Color;
 
