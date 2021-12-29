d14e4ea8f2b73080e6160326fa815e1494943138
KigulaJesse <jkigula@icloud.com>
2021-12-29 14:43:08 +0300

    students searchable

--- src/winBuilder/StudentPanel.java
+++ src/winBuilder/StudentPanel.java
@@ -1,9 +1,25 @@
 package winBuilder;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.RowFilter;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import javax.swing.JScrollPane;
 
 public class StudentPanel extends JPanel {
@@ -17,25 +33,87 @@ public class StudentPanel extends JPanel {
 		
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane);
+		String[] columns = new String[] {"id","Reg No.", "Name", "Age", "Class"};
+        String[][] data = {};
+        ArrayList<String[]> studentList = new ArrayList<String[]>(Arrays.asList(data));  
+        
+        try {
+    		Connection connection = new DbConnection().getDbConnection();
+    		String query = "SELECT * FROM students";
+
+    	    // create the java statement
+    	    Statement studentStatement = connection.createStatement();
+    	      
+    	    // execute the query, and get a java
+    	    ResultSet studentResult = studentStatement.executeQuery(query);
+    	 
+    		
+    	    // iterate through the java
+    	    while (studentResult.next()){
+    	        int id = studentResult.getInt("id");
+    	        String regNum = studentResult.getString("regNum");
+    	        String stuName = studentResult.getString("firstName") +" "+ studentResult.getString("lastName");
+    	        String stuAge = studentResult.getString("stuAge");
+    	        String stuClass = studentResult.getString("stuClass");
+    	        String [] student = {String.valueOf(id), regNum,stuName, stuAge, stuClass };
+    	        studentList.add(student);
+    	    }
+    	    
+    	    data = studentList.toArray(data);
+    	
+    	    studentStatement.close();
+    	}catch(Exception exe) {
+    		System.out.println("here");
+    		exe.printStackTrace();
+    	}
 		
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-			},
-			new String[] {
-				"Reg No.", "First Name", "Last Name", "Gender"
-			}
+			data,
+			columns
 		));
 		scrollPane.setViewportView(table);
+		
+		TableRowSorter<TableModel> sort = new TableRowSorter<>(table.getModel());
+        JTextField textField = new JTextField();
+
+        //set the width of the 3rd column to 200 pixels
+        TableColumnModel columnModel = table.getColumnModel();
+        columnModel.getColumn(2).setPreferredWidth(200);
+    
+        table.setRowSorter(sort);
+
+        JPanel p = new JPanel(new BorderLayout());
+        p.add(new JLabel("Search for a word:"), BorderLayout.WEST);
+        p.add(textField, BorderLayout.CENTER);
+
+        setLayout(new BorderLayout());
+        add(p, BorderLayout.SOUTH);
+        add(new JScrollPane(table), BorderLayout.CENTER);
+
+        textField.getDocument().addDocumentListener(new DocumentListener(){
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String str = textField.getText();
+                if (str.trim().length() == 0) {
+                    sort.setRowFilter(null);
+                } else {
+                    //(?i) means case insensitive search
+                    sort.setRowFilter(RowFilter.regexFilter("(?i)" + str));
+                }
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String str = textField.getText();
+                if (str.trim().length() == 0) {
+                    sort.setRowFilter(null);
+                } else {
+                    sort.setRowFilter(RowFilter.regexFilter("(?i)" + str));
+                }
+            }
+            @Override
+            public void changedUpdate(DocumentEvent e) {}
+        });
 
 	}
 }
