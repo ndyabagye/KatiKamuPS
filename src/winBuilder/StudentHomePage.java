ce653dd9b31b3527bc74112032ee1c7cffef1c0c
kakopo <kiraboismyname@gmail.com>
2021-12-30 10:52:03 +0300

    Timetable

--- src/winBuilder/StudentHomePage.java
+++ src/winBuilder/StudentHomePage.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package winBuilder;
 
 import java.awt.BorderLayout;
@@ -72,69 +71,3 @@ public class StudentHomePage extends JFrame {
 	}
 
 }
-=======
-package winBuilder;
-
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-public class StudentHomePage extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					StudentHomePage frame = new StudentHomePage(1);
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public StudentHomePage(int id) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JButton btnNewButton = new JButton("Timetable");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		menuBar.add(btnNewButton);
-		
-		
-		JButton btnNewButton_1 = new JButton("Results");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		menuBar.add(btnNewButton_1);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
-
-}
->>>>>>> 81fb4d371f9e603338c666be761739155aa9f219
